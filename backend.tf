51b3196d8ebdc5ff0ccda634a11329a3c64e185d
Nikita-Mourya <nikitamourya121995@gmail.com>
2024-11-15 21:08:00 +0530

    [destroying the ec2]

--- backend.tf
+++ backend.tf
@@ -1,37 +1,19 @@
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "my-terraform-state-bucket-demo-nikita"
-#   acl    = "private"
-# }
-
-# resource "aws_dynamodb_table" "terraform_lock" {
-#   name           = "terraform-lock-table"
-#   billing_mode   = "PAY_PER_REQUEST"
-#   hash_key       = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
+# terraform {
+#   backend "s3" {
+#     bucket         = "demokanu"                           # Replace with your S3 bucket name
+#     key            = "state/terraform.tfstate"            # Path to your state file
+#     region         = "us-east-1"                          # AWS region where your S3 and DynamoDB are located                              # Enable encryption for the state file               # DynamoDB table for locking
+#     acl            = "private"                            # Set access control list
 #   }
 # }
 
 
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-demo-nikita"     # Replace with your S3 bucket name
-    key            = "state/terraform.tfstate"            # Path to your state file
-    region         = "us-east-1"                          # AWS region where your S3 and DynamoDB are located
-    encrypt        = true                                 # Enable encryption for the state file
-    dynamodb_table = "terraform-lock-table"               # DynamoDB table for locking
-    acl            = "private"                            # Set access control list
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "hashitalk-demo"
+    workspaces {
+      name = "hashitalk-terraform-demo"
+    }
   }
 }
-
-
-# terraform {
-#   cloud {
-#     organization = "nikitamourya-terraform-org"
-
-#     workspaces {
-#       name = "terraform-cloud-demo1"
-#     }
-#   }
-# }
--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "example" {
-  ami           = "ami-063d43db0594b521b"
-  instance_type = "t3.small"
+# resource "aws_instance" "example" {
+#   ami           = "ami-063d43db0594b521b"
+#   instance_type = "t3.small"
 
-  tags = {
-    Name = "aws-instance-terraform-demo-2"
-  }
-}
+#   tags = {
+#     Name = "aws-instance-terraform-demo-2"
+#   }
+# }
