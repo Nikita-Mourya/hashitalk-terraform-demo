1fc2b7811d170eb19d14ed324a1048b92c65c988
Nikita-Mourya <nikitamourya121995@gmail.com>
2024-11-16 01:34:38 +0530

    cleanup

--- backend.tf
+++ backend.tf
@@ -1,19 +1,19 @@
-terraform {
-  backend "s3" {
-    bucket         = "demokanu"                           # Replace with your S3 bucket name
-    key            = "state/terraform.tfstate"            # Path to your state file
-    region         = "us-east-1"                          # AWS region where your S3 and DynamoDB are located                              # Enable encryption for the state file               # DynamoDB table for locking
-    acl            = "private"                            # Set access control list
-  }
-}
-
-
 # terraform {
-#   cloud {
-#     hostname = "app.terraform.io"
-#     organization = "hashitalk-demo"
-#     workspaces {
-#       name = "hashitalk-terraform-demo"
-#     }
+#   backend "s3" {
+#     bucket         = "demokanu"                           # Replace with your S3 bucket name
+#     key            = "state/terraform.tfstate"            # Path to your state file
+#     region         = "us-east-1"                          # AWS region where your S3 and DynamoDB are located                              # Enable encryption for the state file               # DynamoDB table for locking
+#     acl            = "private"                            # Set access control list
 #   }
 # }
+
+
+terraform {
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "hashitalk-demo"
+    workspaces {
+      name = "hashitalk-terraform-demo"
+    }
+  }
+}
